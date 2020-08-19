4d057dcc0322bda421e10fed8d1f42ffc21ad1da
ThornClub <41445233@qq.com>
2020-08-19 14:56:40 +0800

    修复以Jar包运行xml配置文件无法解析

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
